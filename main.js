69e745b67547a968c015a429e930a7d96891eed5
zihadhossion <zihad41h@gmail.com>
2023-08-22 13:00:21 +0600

    project done

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 const menuItem = document.getElementById("menuItems");
-menuItem.style.maxHeight = "0px";
-function menuToggle() {
-    if (menuItem.style.maxHeight == "0px") {
-        menuItem.style.maxHeight = "200px";
-    } else {
-        menuItem.style.maxHeight = "0px";
-    }
-}
+const menuBtn = document.querySelector(".menu-icon");
+
+menuBtn.addEventListener("click", () => {
+    menuItem.classList.toggle("hidden");
+})
